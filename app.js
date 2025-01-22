c62a00a22effff41d1fb687054338366638dfafb
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-22 14:48:18 +0100

    feat: file importati nella nuova repo. milestone1(WIP)

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors =  require("cors"); 
+const port = 3000;
+app.use(express.json());
+// richiamo le route
+const postsRouters = require("./routers/posts");
+// inserisco handles error 
+const handleError = require("./middleware/handleError");
+// creo assets aperto per le immagini
+app.use(express.static("public"));
+
+app.use(
+    cors({
+    origin: "http://localhost:5173"
+}));
+
+app.use("/posts", postsRouters);
+
+// creo route di base
+app.get("/", (req, res)=>{
+    res.send("sono route di base");
+    
+});
+
+// Dopo tutte le rotte inseriamo il moddleware che gestisce errore
+app.use(handleError);
+
+// creo route di listening
+app.listen(port, ()=>{
+    console.log("server pronto");
+    
+});
---
+++ routers/posts.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+// importo dati da lista dati
+const controllerBacheca = require("../controllers/controllerBacheca");
+const checkFoodExis = require("../middleware/checkFoodExists");
+// index
+router.get(`/`, controllerBacheca.index);
+
+// show
+router.get("/:id", checkFoodExis, controllerBacheca.show);
+// create
+router.post("/", controllerBacheca.create);
+// update
+router.put("/:id",checkFoodExis, controllerBacheca.update);
+
+// modify 
+router.patch("/:id",checkFoodExis, controllerBacheca.modify);
+// delete
+router.delete("/:id",checkFoodExis, controllerBacheca.destroy);
+
+module.exports = router;
