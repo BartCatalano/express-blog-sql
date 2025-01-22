92b2c7eb628c708bd8be21205c0e978dde373277
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-22 15:23:43 +0100

    feat: db creato e cfexist commentato

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const mysql = require(`mysql2`);
+
+const connection = mysql.createConnection({
+    host: `localhost`,
+    user:`root`,
+    password:`root`,
+    database: `blog`
+});
+
+connection.connect((error)=>{
+    if(error) throw err;
+    console.log("connetc to MySql");
+    
+});
+
+export default mysql;
--- middleware/checkFoodExists.js
+++ middleware/checkFoodExists.js
@@ -1,22 +1,22 @@
 const bachecaList = require("../data/data.js");
+next();
+// const checkFoodExist = (req, res, next) => {
 
-const checkFoodExist = (req, res, next) => {
+//     // prendo id da req e lo trsformo in un numero
+//     const foodId = parseInt(req.params.id);
 
-    // prendo id da req e lo trsformo in un numero
-    const foodId = parseInt(req.params.id);
+//     // cerco in array l'oggettto che corrisponde ad id
+//     const food = bachecaList.find((curFood) => curFood.id === foodId );
+// //    creo costrutto if per vedere se trovo l'oggetto
+//    if(food !== undefined) {
+//     next();
+//    } else {
+//     res.statusCode = 404;
+//     res.json({
+//         error: true,
+//         message: "cibo non presente nel menu"
+//     });
+//    }
+// }
 
-    // cerco in array l'oggettto che corrisponde ad id
-    const food = bachecaList.find((curFood) => curFood.id === foodId );
-//    creo costrutto if per vedere se trovo l'oggetto
-   if(food !== undefined) {
-    next();
-   } else {
-    res.statusCode = 404;
-    res.json({
-        error: true,
-        message: "cibo non presente nel menu"
-    });
-   }
-}
-
-  module.exports = checkFoodExist;
+  export default checkFoodExist;
